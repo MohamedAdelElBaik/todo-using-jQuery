f351c80373662c0f507b8825e834169c969c0448
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-02 22:49:21 +0200

    LOGIC : add number for each todo in todo list

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 $('document').ready(function () {
+  let index = 0;
   $('form').submit(function () {
+    index++;
     const todoValue = $('#todoInput').val();
-    $('#todoSection').prepend(`<p>${todoValue}</p>`);
+    $('#todoSection').prepend(`<p><span>${index}</span> ${todoValue}</p>`);
     $('#todoInput').val('');
 
     $('h2').css('background-color', 'red');
